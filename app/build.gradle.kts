af90d7d40688193337620cad29b8e71069d354b4
lucenear <fariasg1988@gmail.com>
2025-11-18 14:38:14 -0300

    Version 1.0 stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kikipdf"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3"
+        versionCode = 5
+        versionName = "1.0"
     }
 
     buildFeatures {
